164d716944ef2d65e2469335c2720c69831bf4e4
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-07-13 16:24:49 +0300

    state guncelleme
    state guncelleme

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import { useSelector, useDispatch } from 'react-redux';
+import { changeName, changeCost, changeDescription } from '../store/slices/formSlice';
 function CourseForm() {
-  return (
-    <div>CourseForm</div>
-  )
+    const dispatch = useDispatch()
+    const { name, description, cost } = useSelector((state) => {
+        return {
+            name: state.form.name,
+            description: state.form.description,
+            cost: state.form.cost
+        };
+    });
+    console.log(name, description, cost);
+    return (
+        <div className="courseForm panel">
+            <h4 className='subtitle is-3'>Kurs ekle</h4>
+            <form>
+                <div className='field-group'>
+                    <div className='field'>
+                        <label className='label'>Ad</label>
+                        <input className='input is-expanded' onChange={(event) => { dispatch(changeName(event.target.value)); }} value={name} />
+                    </div>
+                    <div className='field'>
+                        <label className='label'>Açıklama</label>
+                        <textarea value={description} className='input is-expanded' onChange={(event) => { dispatch(changeDescription(event.target.value)); }} />
+                    </div>
+                    <div className='field'>
+                        <label className='label'>Fiyat</label>
+                        <input value={cost} className='input is-expanded' type='number' onChange={(event) => { dispatch(changeCost(ParseInt(event.target.value))); }} />
+                    </div>
+                </div>
+                <div className='field'>
+                    <button className='button is-primary'>Kaydet</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default CourseForm
--- src/store/slices/formSlice.js
+++ src/store/slices/formSlice.js
@@ -8,8 +8,17 @@ const formSlice = createSlice({
         cost: 0
     },
     reducers: {
-
-    }
+        changeName(state, action) {
+            state.name = action.payload;
+        },
+        changeDescription(state, action) {
+            state.description = action.payload;
+        },
+        changeCost(state, action) {
+            state.cost = action.payload;
+        },
+    },
 });
 
+export const { changeName, changeDescription, changeCost } = formSlice.actions;
 export const formReducer = formSlice.reducer;
