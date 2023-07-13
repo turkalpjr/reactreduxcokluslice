27451279762dc77735c46f5170077fd6d6771fa6
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-07-13 15:21:27 +0300

    first commit
    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import CourseForm from './components/CourseForm';
+import CourseList from './components/CourseList';
+import CourseSearch from './components/CourseSearch';
+import CourseValue from './components/CourseValue';
 
 function App() {
   return (
-    <>
-      test
-    </>
+    <div className='container is-fluid'>
+      <CourseForm />
+      <CourseSearch />
+      <CourseList />
+      <CourseValue />
+    </div>
   );
 }
-
 export default App;
---
+++ src/store/slices/courseSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const courseSlice = createSlice({
+    name: 'form',
+    initialState: {
+        searchTerm: '',
+        data: []
+    },
+    reducers: {
+
+    }
+});
+
+export const courseReducer = courseSlice.reducer;
